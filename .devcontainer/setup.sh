dcf3da9cdd34b5018ddfa1e23d6753243bd1b272
warjiang <1096409085@qq.com>
2025-03-28 00:50:46 -0700

    feat: update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -9,33 +9,18 @@ export http_proxy=http://192.168.20.3:7890
 export all_proxy=http://192.168.20.3:7890
 
 # install oh-my-zsh & powerlevel10k theme
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+cd $HOME && curl -fsSLO https://raw.githubusercontent.com/romkatv/dotfiles-public/master/.purepower
 
 unset https_proxy
 unset http_proxy
 unset all_proxy
 
 
-
-tee -a ~/.zshrc <<EOF
-export proxy_client="192.168.20.3"
-export proxy_port="7890"
-function proxy() {
-    ip=${proxy_client}
-    port=${proxy_port}
-    if [ "$1" = "on" ]; then
-        export https_proxy=http://$proxy_client:$proxy_port
-        export http_proxy=http://$proxy_client:$proxy_port
-        export all_proxy=http://$proxy_client:$proxy_port
-        echo proxy on
-    else
-        unset https_proxy
-        unset http_proxy
-        unset all_proxy
-        echo proxy off
-    fi
-}
-EOF
+cp /tmp/.zshrc ~/.zshrc
 
 echo "Setup complete"
